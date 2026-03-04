ce44e5a88877d975408eac0643369d413452b133
billgrah48 <billy_graham@hotmail.co.uk>
2026-03-04 15:36:29 +0000

    HTML, CSS, HEADER, POSTMAN AND API

--- src/main/java/com/example/course_app/HelloController.java
+++ src/main/java/com/example/course_app/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.course_app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from Spring!");
+        model.addAttribute("body", "This is my page.");
         return "hello";
     }
+
+
 }
 
